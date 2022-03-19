26e8788d925289b1b379dcbbaa047219d5a9dd35
CathrineChernogor <chernogor_k@mail.ru>
2022-03-19 19:14:55 +0800

    organize app.ry: add blueprint and separate db

---
+++ app/start_view.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from .route_logic.route_building import build_route
+from .db import mysql
+
+start = Blueprint('start', __name__)
+
+
+@start.route('/')
+def index():
+    return render_template('start_page.html')
+
+
+@start.route('/generated', methods=['GET', 'POST'])
+def generated():
+    if request.method == 'GET':
+        return render_template('generated.html')
+    else:
+        build_route(request.json)
+        return redirect(url_for('building'))
+
+
+@start.route('/predefined', methods=['GET', 'POST'])
+def predefined():
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * from predefinedroutes;")
+        data = cursor.fetchall()
+        return render_template('predefined.html', routes=data)
+    elif request.method == 'POST':
+        # print('received json', request.json)
+        build_route(request.json)
+        return redirect(url_for('building'))
+
+
+@start.route('/custom', methods=['GET', 'POST'])
+def custom():
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT e.id, e.name, r.name from entities as e, rooms as r where e.roomId = r.id;")
+        data = cursor.fetchall()
+        dict_data = {}
+
+        for it in data:
+            if it[2] in dict_data.keys():
+                value = dict_data.get(it[2])
+                value.append([it[0], it[1]])
+            else:
+                dict_data[it[2]] = [[it[0], it[1]]]
+
+        return render_template('custom.html', rooms=dict_data)
+    elif request.method == 'POST':
+        # print('received json', request.json)
+
+        build_route(request.json)
+        return redirect(url_for('building'))
+
---
+++ run.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, Blueprint
+from data.config import HOST, USER, DBNAME, PASSWORD
+from app.start_view import start
+from app.db import mysql
+
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = USER
+app.config['MYSQL_DATABASE_PASSWORD'] = PASSWORD
+app.config['MYSQL_DATABASE_DB'] = DBNAME
+app.config['MYSQL_DATABASE_HOST'] = HOST
+mysql.init_app(app)
+
+
+app.register_blueprint(start)
+
+
+@app.route('/building')
+def building():
+    print("bulding")
+    return 'Your route is building...'
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error=error), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
