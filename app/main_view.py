4efd7075ebbd446acd438c0667ae8aefc55bd22c
CathrineChernogor <chernogor_k@mail.ru>
2022-03-22 00:25:50 +0800

    add stat menu and universal table template

--- app/main_view.py
+++ app/main_view.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
+from .db import mysql
+from .logic.stat_computing import get_entities_rating, get_rooms_visiting
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def welcome():
     return render_template('welcome.html')
+
 
 @main.route('/guide')
 def guide():
@@ -23,11 +25,32 @@ def gallery():
 
 
 @main.route('/portal')
-def portal(): 
+def portal():
     return 'portal'
 
 
 @main.route('/statistics')
-def statistics(): 
-    return 'statistics'
+def statistics_menu():
+    return render_template('statistics.html')
 
+
+@main.route('/rooms_visiting')
+def rooms_visiting():
+    data = get_rooms_visiting()
+    return render_template('list.html',
+                           data={},
+                           thead=[],
+                           name="Посещаймость залов",
+                           description="""Посещения по залам в течение дня """)
+
+
+@main.route('/entities_rating')
+def entities_rating():
+    data = get_entities_rating()
+    head = list(data.values())[0].keys()
+    return render_template('list.html',
+                           data=data,
+                           thead=head,
+                           name="Рейтинг экспонатов",
+                           description="""Общий список экспонатов по посещаемости 
+                           и по средней общей оценке""")
