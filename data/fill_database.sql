60871fc1da400ec5b716a8da1b79c3c12210d06b
CathrineChernogor <chernogor_k@mail.ru>
2022-03-25 18:11:30 +0800

    add paths for markers and overlays in mysql

--- data/fill_database.sql
+++ data/fill_database.sql
@@ -5,15 +5,15 @@ INSERT INTO `Rooms` (`id`, `name`,`exsStr`) VALUES
 (4, 'Общий зал', '9');
 
 INSERT INTO `Entities` (`id`, `name`,`roomId`, `targetUri`, `overlayType`, `overlayUri`) VALUES 
-(1, 'Экспонат 1', '1', 'None', 1, 'None'),
-(2, 'Экспонат 2', '1', 'None', 1, 'None'),
-(3, 'Экспонат 3', '2', 'None', 1, 'None'),
-(4, 'Экспонат 4', '2', 'None', 1, 'None'),
-(5, 'Экспонат 5', '3', 'None', 1, 'None'),
-(6, 'Экспонат 6', '3', 'None', 1, 'None'),
-(7, 'Экспонат 7', '3', 'None', 1, 'None'),
-(8, 'Экспонат 8', '3', 'None', 1, 'None'),
-(9, 'Сувенирная лавка', '4', 'None', 0, 'None');
+(1, 'Экспонат 1', '1', '/static/assets/markers/zpt/1.zpt', 1, '/static/assets/overlays/1.mp4'),
+(2, 'Экспонат 2', '1', '/static/assets/markers/zpt/2.zpt', 1, '/static/assets/overlays/2.mp4'),
+(3, 'Экспонат 3', '2', '/static/assets/markers/zpt/3.zpt', 2, '/static/assets/overlays/3.jpg'),
+(4, 'Экспонат 4', '2', '/static/assets/markers/zpt/4.zpt', 2, '/static/assets/overlays/4.jpg'),
+(5, 'Экспонат 5', '3', '/static/assets/markers/zpt/5.zpt', 3, '/static/assets/overlays/5.glb'),
+(6, 'Экспонат 6', '3', '/static/assets/markers/zpt/6.zpt', 3, '/static/assets/overlays/6.glb'),
+(7, 'Экспонат 7', '3', '/static/assets/markers/zpt/7.zpt', 3, '/static/assets/overlays/7.glb'),
+(8, 'Экспонат 8', '3', '/static/assets/markers/zpt/8.zpt', 3, '/static/assets/overlays/8.glb'),
+(9, 'Сувенирная лавка', '4', '/static/assets/markers/zpt/9.zpt', 3, '/static/assets/overlays/9_1.glb');
 
 INSERT INTO `Map` (`id`, `name`,`jsonUri`) VALUES (1, 'Музей капля Байкала', 'data/matrix.json');
 
