b7e864717d253471fedb1ed0d996f355b7f68956
CathrineChernogor <chernogor_k@mail.ru>
2022-03-25 18:41:52 +0800

    add glb model entities rendering for face tracking

---
+++ app/guide_view.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from .logic.route_building import build_route
+from .db import mysql
+
+guide = Blueprint('guide', __name__)
+
+
+@guide.route('/search/<entity_id>')
+def search(entity_id):
+    pass
+
+
+@guide.route('/message/<entity_id>')
+def message(entity_id):
+    pass
+
+
+@guide.route('/show/<entity_id>')
+def show(entity_id):
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql_query = f"""SELECT name, targetUri, overlayType, overlayUri 
+                    FROM Entities WHERE id = {entity_id};"""
+
+    cursor.execute(sql_query)
+    data = cursor.fetchall()
+
+    name, targetUri, overlayType, overlayUri = data[0]
+
+    template = 'welcome.html'
+
+    if overlayType == 3:
+        template = 'overlay_model.html'
+    if overlayType == 2:
+        template = 'overlay_img.html'
+    if overlayType == 1:
+        template = 'overlay_video.html'
+
+    return render_template(template,
+                           name=name,
+                           marker_src=targetUri,
+                           model_src=overlayUri)
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, Blueprint
 from data.config import HOST, USER, DBNAME, PASSWORD
 from app.start_view import start
 from app.main_view import main
+from app.guide_view import guide
 from app.stat_view import stat
 from app.db import mysql
 
@@ -17,12 +18,7 @@ mysql.init_app(app)
 app.register_blueprint(start)
 app.register_blueprint(main)
 app.register_blueprint(stat)
-
-
-@app.route('/building')
-def building():
-    print("bulding")
-    return 'Your route is building...'
+app.register_blueprint(guide)
 
 
 @app.errorhandler(404)
