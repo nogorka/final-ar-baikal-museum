e18f9a878707eaabd6c5eaff61629f494b0c0bc2
CathrineChernogor <chernogor_k@mail.ru>
2022-03-25 14:52:07 +0800

    fix rendering (import jsonify)

--- app/start_view.py
+++ app/start_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from .logic.route_building import build_route
 from .db import mysql
 
